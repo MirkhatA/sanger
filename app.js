2a12f3289190644af70ac4fd26918b8f13f53722
mirkek <asen.mirkhat@bk.ru>
2023-01-26 19:30:59 +0600

    morgan dep commit

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 //routes
 const { getItems } = require("./routes/item");
 
-app.get("/", getItems);
+const myMiddleware = (req, res, next) => {
+  console.log("test");
+  next();
+};
+
+// middleware
+app.use(morgan("dev"));
+app.use(myMiddleware);
+
+app.get("/getItems", getItems);
 
 const port = 4000;
 app.listen(port, () => {
